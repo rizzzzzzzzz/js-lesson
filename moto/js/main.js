63905e6ddb5b146bf2b9405d2e3ea88c92419e04
rizzz <rizvansultigov@gmail.com>
2017-11-29 00:00:23 +0300

    no message

--- moto/js/main.js
+++ moto/js/main.js
@@ -1,9 +1,10 @@
 $(function(){
-	$('img').each(function(){
-		$(this).on('click', function(){
-			if($(this).height() > 50 && $(this).height() < 100){
-				$(this).toggle(1000)
-			};
-		});
-	});
+	$('#moto_models > li').each(function(){
+		$(this).mouseover(function(){
+			$(this).css({
+				background : '#1f233c',
+				color : '#fff'
+			})			
+		})
+	})
 });
